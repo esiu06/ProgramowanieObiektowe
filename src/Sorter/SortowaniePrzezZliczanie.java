40b639465f22f367dd2f65c9e56c27126cf6c256
Esiu-Komputer\Esiu <pawel.esmanowicz@wp.pl>
2017-08-29 09:06:20 +0200

    sorter dokończenie

--- src/Sorter/SortowaniePrzezZliczanie.java
+++ src/Sorter/SortowaniePrzezZliczanie.java
@@ -3,9 +3,30 @@ package Sorter;
 public class SortowaniePrzezZliczanie implements ISort {
     @Override
     public int[] sort(int[] tab) {
+        int max = getMax(tab);
+        int[]tabZliczen = new int[max+1];
         for (int i = 0; i <tab.length; i++) {
-
+            tabZliczen[tab[i]]++;
         }
-        return tab;
+        int[] tabZwrotow = new int[tab.length];
+        int iterator = 0;
+        for (int i = 0; i <tabZliczen.length ; i++) {
+            while (tabZliczen[i]>0){
+                tabZliczen[iterator]=i;
+                tabZliczen[i]--;
+                iterator++;
+            }
+        }
+        return tabZwrotow;
+    }
+
+    private int getMax(int[] tab) {
+        int max = 0;
+        for (int i = 0; i < tab.length; i++) {
+            if(max<tab[i]){
+                max=tab[i];
+            }
+        }
+        return max;
     }
 }
